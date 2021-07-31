e8ef824f87ad381d77e1afd99d554bc3e7c7841f
Sabitra Sankalp <panigrahi.1@iitj.ac.in>
2021-07-31 12:53:55 +0530

    edited snips file wrt config

--- snips.sh
+++ snips.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-
+#!/usr/bin/env sh
 #script to open ultisnippets for reference
 
-function open_snip_file()
+open_snip_file()
 {
 	nvim $SNIPS_DIR$1.snippets
 }
 
-SNIPS_DIR=/Users/user/.config/nvim/plugged/vim-snippets/UltiSnips/
+SNIPS_DIR=$HOME/.config/nvim/plugged/vim-snippets/UltiSnips/
 open_snip_file $1
