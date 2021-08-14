22e4e28ea768957f03fd37a4e34712df0b8336e6
Sabitra <b114032@iiit-bh.ac.in>
2021-08-14 13:55:53 +0530

    edited mcd

--- mcd.sh
+++ mcd.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+##!/usr/bin/env sh
 #script to make a directory and change into it.
 
-function mcd ()
+mcd ()
 {
     mkdir -p $1
+}
+
+chdr ()
+{
     cd $1
 }
 
 #input is relative path to file
 #$1 is directory string
 mcd $1
+chdr $1
